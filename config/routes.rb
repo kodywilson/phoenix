afdca9a1fcedb5180edef3aff4472dbb0ab60be3
Kody Wilson <kodywilson@gmail.com>
2014-06-03 14:31:28 -0700

    Added root route to pull up fires by default.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :fires
+  root :to => redirect('/fires')
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
