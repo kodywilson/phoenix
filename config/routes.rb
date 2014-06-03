b996583ebf5306034296b883df0c1a4a5c93b99a
Kody Wilson <kodywilson@gmail.com>
2014-06-03 14:36:11 -0700

    Used scaffold to generate MVCr's for comments to be added to fires.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :comments
+
   resources :fires
   root :to => redirect('/fires')
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140603210801) do
+ActiveRecord::Schema.define(version: 20140603213453) do
+
+  create_table "comments", force: true do |t|
+    t.string   "user_name"
+    t.text     "body"
+    t.integer  "fire_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "fires", force: true do |t|
     t.string   "name"
