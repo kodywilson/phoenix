3bcea675b16175219e6df06ace64d9038cabe60d
Kody Wilson <kodywilson@gmail.com>
2014-06-03 20:43:32 -0700

    Trying to get rspec to work, I think I may need to generate some stuff since I accidentally generated a lot with scaffolds before I added rspec to the Gemfile.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
+#require 'app/models/user.rb'
 
-describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+#describe User do
+#  it "has an email address" do
+#    test_user = User.new
+#    test_user.email.should be_true
+#  end
+#end
